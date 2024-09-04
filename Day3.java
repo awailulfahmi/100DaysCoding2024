9db92f9dc5d04b8847328af55387a09146398060
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-04 14:08:17 +0800

    3 print

---
+++ Day3.java
@@ -0,0 +1,8 @@
+
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Awailul Fahmi");   
+        System.out.println("Kelas : Informaatika A");   
+        System.out.println("Nim : D0224042");   
+    }
+    }
