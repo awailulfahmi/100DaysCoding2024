f1298d449955dd98cd7f818ae9053e306fb707f1
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-07 12:49:10 +0800

    Variabel

---
+++ Day6.java
@@ -0,0 +1,14 @@
+
+package day.pkg6;
+
+public class Day6 {
+    public static void main(String[] args) {
+    String nama;
+    nama = "Muhammad awailul fahmi";
+    char kelas;
+    kelas = 'a';
+        System.out.printf("nama: %s\n", nama);
+        System.out.println("kelas:"+kelas);
+        System.out.print("belajar coding sangat menyenangkan sekali\n");
+    }
+}
