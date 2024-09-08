c00cc4971921ae017b67d17a346e1649ebb532cf
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-08 18:44:53 +0800

    Variabel

---
+++ Day7.java
@@ -0,0 +1,14 @@
+
+package javaapplication12;
+
+public class JavaApplication12 {
+    public static void main(String[] args) {
+        String nama= "Muhammad awailul fahmi";
+        int umur= 18;
+        char kls= 'a';
+        System.out.println("Nama : "+ nama);
+        System.out.println("Umur : "+ umur);
+        System.out.println("Kelas : "+ kls);
+    }
+    
+}
