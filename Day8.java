ac86e3738d49a05c25ef152202deb609aa566fd3
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-09 15:29:54 +0800

    Variabel

---
+++ Day8.java
@@ -0,0 +1,19 @@
+
+package day.pkg8;
+
+public class Day8 {
+    public static void main(String[] args) {
+        String nama = "muhammad awailul fahmi";
+        double nilai = 90.00;
+        nilai = 99.99;
+        int predikat= 1;
+        predikat= 1;
+        
+         System.out.println("nama :"+ nama);       
+         System.out.println("nilai coding :"+ nilai);       
+         System.out.println("rengking :"+ predikat);
+         System.out.print("coding menyenangkan\n");
+                
+                
+    }
+}
