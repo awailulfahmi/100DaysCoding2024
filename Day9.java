feb9110ce1cf8b77a918c0a5be7516ebd935aa6e
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-10 12:59:52 +0800

    Variabel

---
+++ Day9.java
@@ -0,0 +1,12 @@
+package day9;
+
+public class Day9 {
+    public static void main(String[] args) {
+    final int nilai = 666;
+    final float IPK= 4.0f ;
+    
+        System.out.println("Nilai matkul =" + nilai );
+        System.out.println("Predikat yang di peroleh =" + IPK );
+            
+    }
+    }
