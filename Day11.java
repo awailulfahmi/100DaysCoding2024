ea5b720138b49bccb1d791e0e151bc87f102545a
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-12 08:45:34 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,9 @@
+package day.pkg11;
+
+public class Day11 {
+    public static void main(String[] args) {
+   short angka = 1;
+   //range -32768 to 32767
+        System.out.println("angka :"+ angka);
+    }
+}
