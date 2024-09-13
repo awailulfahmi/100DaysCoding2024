e716a7ee3309926f99f4962cd6010ed9746b63f2
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-13 13:34:50 +0800

    int

---
+++ Day12.java
@@ -0,0 +1,9 @@
+package day12;
+
+public class Day12 {
+    public static void main(String[] args) {
+        int umur = 18;
+        //rentan angka -707 sampai dengan 707
+        System.out.println("umurku = " + umur);
+    }
+}
