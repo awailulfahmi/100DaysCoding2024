7f8a28b6697ca8cf4738ca457ed100e0847996a8
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-15 16:26:35 +0800

    long

--- Day14.java
+++ Day14.java
@@ -2,8 +2,25 @@ package day14;
 
 public class Day14 {
     public static void main(String[] args) {
-        long angka = 999;
-        //di gunakan untuk menyimpan nilai bilangan bulat yang sangat besar
-        System.out.println("bilangan :" + angka);
+        String nama = "awal";
+        long umur = 999;
+        boolean benar = false;
+        float suhu = 9.7f;
+        char kelas = 'a';
+        int tanggal = 15;
+        byte tgl;
+        tgl = 16;
+        String alamat = "jln hj andi depu";
+        //tipe data long di gunakan untuk menyimpan nilai bilangan
+        //bulat yang sangat besar
+        System.out.printf("nama panggilan : %s\n" ,nama);
+        System.out.println("bilangan :" + umur);
+        System.out.println("true : "+ false);
+        System.out.printf("suhu udara : %f\n" ,suhu);
+        System.out.printf("kelas : %c\n" ,kelas);
+        System.out.println("tanggal sekarang " + tanggal);
+        System.out.println("tanggal besok " + tgl);
+        System.out.printf("alamat sy : %s\n" ,alamat);
+        System.out.print("saya suka coding\n");
     }
 }
