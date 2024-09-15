143ac8f2c3aad0c7808d6bf58fa8b6ce49d27787
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-15 10:11:58 +0800

    long

---
+++ Day14.java
@@ -0,0 +1,9 @@
+package day14;
+
+public class Day14 {
+    public static void main(String[] args) {
+        long angka = 999;
+        //di gunakan untuk menyimpan nilai bilangan bulat yang sangat besar
+        System.out.println("bilangan :" + angka);
+    }
+}
