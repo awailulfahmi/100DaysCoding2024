5906320f07d8feae71eb3f4c0f4f662d3f7ba3a1
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-18 08:08:20 +0800

    float

---
+++ Day16.java
@@ -0,0 +1,20 @@
+package day16;
+
+public class Day16 {
+    public static void main(String[] args) {
+        String nama = "muhammad awailul fahmi";
+        String kelas = "informatika A";
+        float suhu = 1.8f;
+        suhu = 1.91f;
+        float angka = -18f;
+        //tipe data yang digunakan untuk mempresentasikan atau 
+        //mngefitentifikasikan bilangan numerik, khusunya angka - angka
+        //pecahan atau desimal dalam pemograman
+        
+        System.out.printf("Nama lengkap :%s\n",nama);
+        System.out.printf("Kelas :%s\n",kelas);
+        System.out.printf("Umur :%f\n",suhu);
+        System.out.println("angka :"+ angka);
+       
+    }
+}
