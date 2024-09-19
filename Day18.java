ec3f99980a51e8b727179801ff069be5e9009222
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-19 20:09:18 +0800

    boolean

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package day18;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+    boolean kalimat = true;
+    boolean kalimat_2 = false;
+        System.out.println("aku senang belajar"+kalimat);
+        System.out.println("aku tidak senang belajar"+kalimat_2);
+        
+    }
+    
+}
