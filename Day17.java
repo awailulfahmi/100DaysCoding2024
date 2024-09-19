4ee53ee10ba1d664c4365262fa4270aefd60e35c
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-19 19:39:41 +0800

    tipe data char

---
+++ Day17.java
@@ -0,0 +1,12 @@
+package day.pkg17;
+
+public class Day17 {
+    public static void main(String[] args) {
+        String nama = "muhammad awailul fahmi";
+        char kelas = 'a';
+        float desimal = 1.9f;
+        System.out.printf("nama = %s\n",nama);
+        System.out.println("kelas "+ kelas);
+        System.out.printf("desimal = "+ desimal);
+    }
+}
