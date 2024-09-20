5daa5303fadf8f84e1e485264749cd14c56812bb
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-20 20:43:16 +0800

    String

---
+++ Day19.java
@@ -0,0 +1,11 @@
+
+package day19;
+
+public class Day19 {
+    public static void main(String[] args) {
+        String nama = "muhammad awailul fahmi";
+        String alamat = "jln haji andi depu";
+        System.out.println("Nama lengkap "+nama);
+        System.out.println("Alamat "+alamat);
+    }
+}
