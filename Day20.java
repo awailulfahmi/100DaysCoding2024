b4727555535ff9735a3d4d23038b5c54f77c0907
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-21 22:49:54 +0800

    Byte

---
+++ Day20.java
@@ -0,0 +1,13 @@
+package day20;
+
+public class Day20 {
+    public static void main(String[] args) {
+       String nama ="muhammad awilul fahmi";
+        Byte umur = 18;
+        int angka =19;
+        angka+=5;
+        System.out.println("nama"+nama);
+        System.out.println("umur"+umur);
+        System.out.println("angka"+angka);
+    }
+}
