78e66149a83384206720f9cedd889f6268fbbad6
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-22 11:54:36 +0800

    Short

---
+++ Day21.java
@@ -0,0 +1,13 @@
+
+package day.pkg21;
+
+public class Day21 {
+    public static void main(String[] args) {
+        Short angka = 99;
+        angka = 100;
+        Short angka_2 = 999;
+        System.out.println("angka"+angka);
+        System.out.println(angka_2);
+    }
+    
+}
