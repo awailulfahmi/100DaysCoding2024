40214098e8598f7cbe788f590d4c4e1adbd44bd7
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-23 21:51:31 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,11 @@
+
+package day.pkg22;
+
+public class Day22 {
+    public static void main(String[] args) {
+        Integer angka = 29;
+        Integer angka_2 = 30;
+        System.out.println(angka);
+        System.out.println(angka_2);
+    }
+}
