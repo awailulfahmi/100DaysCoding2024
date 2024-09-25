bb122a19a1ea399a89c915efbda3aac8cb07d2e1
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-25 21:26:12 +0800

    Float

---
+++ Day24.java
@@ -0,0 +1,11 @@
+
+package day24;
+
+public class Day24 {
+public static void main(String[]args){
+    String nama = "muhammad awailul fahmi";
+    Float a = 1.8f;
+    System.out.printf("nama :%s", nama);
+    System.out.printf("suhu :%f",a);
+}
+}
