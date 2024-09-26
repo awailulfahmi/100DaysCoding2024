2657d5668556f1783bdd1dffd628821b0e297dcd
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-26 22:48:07 +0800

    double

---
+++ Day25.java
@@ -0,0 +1,15 @@
+package javaapplication40;
+
+public class JavaApplication40 {
+public static void main(String[]args){
+    String nama = "muhammad awailul fahmi";
+    Integer umur =19;
+Double angka = 1.0;
+Float angka1 =1.9f;
+System.out.printf("nama :%s\n",nama);
+System.out.println("umur saya :"+angka);
+System.out.println("Angka :"+angka);
+System.out.printf("angka 1:%f\n",angka1);
+ 
+} 
+}
