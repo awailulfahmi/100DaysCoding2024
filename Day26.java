e56ae975f0fbbc9efdea05fac674ef6ba19633cd
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-27 21:34:52 +0800

    Character

---
+++ Day26.java
@@ -0,0 +1,9 @@
+package day.pkg27;
+public class Day27{
+    public static void main(String[]args){
+        Character huruf = 'a';
+        Integer angka = 18;
+        System.out.println(huruf);
+        System.out.println(angka);
+    }
+}
