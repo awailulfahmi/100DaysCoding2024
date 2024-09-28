329deb2ad99e7c8dacef7b13a13eb6fec2460587
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-28 21:44:18 +0800

    Evaluasi besar

---
+++ Day27.java
@@ -0,0 +1,14 @@
+
+package DataHewan;
+
+public class CatatanHewan {
+
+    public static void main(String[] args) {
+    System.out.println("Selamat datang di catatan data hewan kebun binatang Wild Park!");
+    int jumlahHewan = 50;
+    String namaHewan = "harimau";
+    namaHewan = "Singa";
+    Byte umurHewan = 5;
+    System.out.printf("Hewan bernama %s memiliki umur %d tahun. Jumlah hewan yang tersedia saat ini adalah %d",namaHewan,umurHewan,jumlahHewan);
+}
+}
