337204e2f455f940e1976780669b06ebb91cd97f
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-29 23:05:08 +0800

    Boolean

---
+++ Day28.java
@@ -0,0 +1,10 @@
+public class Day28 {
+public static void main(String[]args){
+    Boolean aku =  false;
+Boolean dia = true;
+Boolean sama=dia=aku;
+    System.out.println("aku"+aku);
+    System.out.println("dia"+dia);
+    System.out.println("apakah bisa bersama"+sama);
+}
+}
