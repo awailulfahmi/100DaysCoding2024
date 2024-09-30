c6e3c8724a158af05f04c0b1fe107eabf7c94daf
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-09-30 22:22:34 +0800

    BigInteger

---
+++ Day29.java
@@ -0,0 +1,14 @@
+package day29;
+import java.math.BigInteger;
+public class Day29 {
+
+    public static void main(String[] args) {
+BigInteger a = new BigInteger("12");
+BigInteger c = new BigInteger("18");
+BigInteger tambah = a.add(c);
+BigInteger kurang = a.subtract(c);
+        System.out.println("Pertambahan : "+ tambah);
+        System.out.println("Pengurangan : "+ kurang);
+    }
+    
+}
