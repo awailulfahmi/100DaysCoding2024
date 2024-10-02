c650dc857db161d3fc05c6c8bd9379112d9ae909
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-02 23:17:35 +0800

    Konversi primitif ke string

---
+++ Day32.java
@@ -0,0 +1,11 @@
+
+public class Day32 {
+    public static void main(String[] args) {
+        int a = 18;
+        char d ='a';
+        String b = String.valueOf(a);
+        String c = String.valueOf(d);
+        System.out.println("Konversi tipe data primitif ke string "+b);
+        System.out.println("Konversi tipe data primitif ke string "+c);
+    }
+}
