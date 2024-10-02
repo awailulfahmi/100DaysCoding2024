591ee1a474dec8053221f24e816152047b4425a8
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-02 21:26:05 +0800

    Mengubah string

---
+++ Day31.java
@@ -0,0 +1,8 @@
+
+public class Day31 {
+    public static void main(String[] args) {
+String a = "123";
+byte b = Byte.parseByte(a);
+        System.out.println("Mengubah String ke tipe daya byte "+b);
+    }
+}
