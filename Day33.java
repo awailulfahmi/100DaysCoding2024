06a16dd390a7cb1df1472ebb9e20f5605a98d7ae
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-04 20:43:17 +0800

    Type casting

---
+++ Day33.java
@@ -0,0 +1,9 @@
+
+public class Day33 {
+    public static void main(String[]args){
+        int numInt = 190;
+        float numFloat = numInt;
+        System.out.println("Nilai int : "+numInt);
+        System.out.println("Nilai float : "+numFloat);
+    }
+}
