2afb2116b8f5207fef67b274ca3c92413f095bad
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-04 21:01:55 +0800

    menginput dari keybord

---
+++ Day34.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day34 {
+    public static void main(String[]args){
+        Scanner ab = new Scanner (System.in);
+        System.out.println("masukan nama anda");
+        String b = ab.nextLine();
+        System.out.println("masukan umur anda");
+        int a = ab.nextInt();
+        System.out.println("terima kasih");
+        
+    }
+}
