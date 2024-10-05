d178022f38675b41d6b65b0f97c17062092a0c1a
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-05 19:05:39 +0800

    menginput dari keybord

--- Day34.java
+++ Day34.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 public class Day34 {
     public static void main(String[]args){
         Scanner ab = new Scanner (System.in);
-        System.out.println("masukan nama anda");
-        String b = ab.nextLine();
-        System.out.println("masukan umur anda");
-        int a = ab.nextInt();
-        System.out.println("terima kasih");
+        //membuat scanner
+        System.out.println("masukan gaji pokok");
+        double gajipokok = ab.nextDouble();
+        System.out.println("masukan jam lembur");
+        int jamlembur = ab.nextInt();
+        int bonuslembur = 10000;
+        double total = bonuslembur * jamlembur;
+        double gajibersih = gajipokok + total;
+        System.out.println("gaji bersih yang diterima Rp"+gajibersih);
         
     }
 }
