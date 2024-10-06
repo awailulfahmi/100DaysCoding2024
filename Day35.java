2a2628589a12729adac1fc0ee40bb126a400804e
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-06 22:57:46 +0800

    penjumlahan dan pengurangan

---
+++ Day35.java
@@ -0,0 +1,8 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("hasil penjumlahan dari"+(a+b));
+        System.out.println("hasil pengurangan dari"+(a-b));
+    }
+}
