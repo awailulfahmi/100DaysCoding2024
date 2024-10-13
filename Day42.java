bda425a095a60f9450c0e966b1f457ceb34507af
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-13 23:31:45 +0800

    sama dengan

---
+++ Day42.java
@@ -0,0 +1,17 @@
+
+package day42;
+import java.util.Scanner;
+public class Day42 {
+
+    public static void main(String[] args) {
+Scanner awal = new Scanner(System.in);
+        System.out.println("masukan angka");
+int a = awal.nextInt();
+        System.out.println("masukan angka kedua");
+        int b = awal.nextInt();
+        
+        System.out.println("angka a sama dengan angka b"+(a==b));
+        System.out.println("angka a tidak sama dengan angka b"+(a!=b));
+    }
+    
+}
