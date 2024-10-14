7b6f56df77e3c233ee4355f29d92aa99fe92c5f4
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-14 11:45:06 +0800

    sama dengan lebih kecil sama dengan lebih besar

---
+++ Day43.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class Day43 {
+    public static void main(String[] args) {
+Scanner awal = new Scanner(System.in);
+        System.out.println("masukan angka");
+int a = awal.nextInt();
+        System.out.println("masukan angka kedua");
+        int b = awal.nextInt();
+        
+        System.out.println("angka a lebih besar dari angka b "+(a>=b));
+        System.out.println("angka a lebih kecil dari angka b "+(a<=b));
+    }
+    
+}
