46f6f3c5656cc6838d90d031e976b938652b7d05
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-17 21:02:52 +0800

    kebalikan

---
+++ Day46.java
@@ -0,0 +1,12 @@
+
+public class Day46 {
+    public static void main(String[] args) {
+        boolean bb= 2 < 7;
+        boolean ac = 2>=5;
+        System.out.println(!bb);
+        System.out.println(!ac);
+
+
+        
+    }
+}
