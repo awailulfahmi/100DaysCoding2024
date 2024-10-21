10344fb2f6f58d9decc5c009a4dbb33baf501bc6
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-21 22:30:11 +0800

    Create Day49.java

---
+++ Day49.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day49 {
+   public static void main(String[]arngs) {
+       Scanner ab = new Scanner(System.in);
+       System.out.println("Masukan pasword");
+       String a = ab.nextLine();
+        
+        if (a.equalsIgnoreCase("Muhammad awailul fahmi")){
+            System.out.println("umur: 18thn");
+            
+         }
+        else if(a.equals("awal")){
+            System.out.println("welcome");
+            System.out.println("my name is awal");
+            
+}
+    }
+    }
+    
