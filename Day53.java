f17e081578b322758d44ea07aaaa9cd2adc86f11
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-24 21:33:56 +0800

    for

---
+++ Day53.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Day53 {
+    public static void main(String[]arngs){
+        Scanner ab = new Scanner (System.in);
+        int a = ab.nextInt();
+        for (int b = 0 ; b<=100000 ; b++ ){
+            System.out.println(b);
+        }
+        
+    }
+}
