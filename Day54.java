700b6d6976c98fe209754e723f1061f3c7d593cb
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-25 16:01:10 +0800

    for incretmen

---
+++ Day54.java
@@ -0,0 +1,8 @@
+public class Day54 {
+    public static void main(String[]arngs){
+        for (int i = 0 ; i<=100 ; i++ ){
+            System.out.println("Nilai"+i);
+        }
+        
+    }
+}
