9992c15d294212a82e44bbeea6934099cedc146a
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-26 19:48:27 +0800

    for decretmen

---
+++ Day56.java
@@ -0,0 +1,8 @@
+public class Day54 {
+    public static void main(String[]arngs){
+        for (int i = 56 ; i>0 ; i-- ){
+            System.out.println("Codingan Day"+i);
+        }
+        
+    }
+}
