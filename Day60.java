f81032fc753947ef70f18e1421fa831a51952084
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-10-31 00:43:25 +0800

    Looping : do while

---
+++ Day60.java
@@ -0,0 +1,11 @@
+public class Day60 {
+    public static void main(String[]arangs){
+        int angka = 10;
+        do{
+            System.out.println("tew tweee "+angka);
+            angka++;
+            
+        }while(angka<=100);
+        
+    }
+
