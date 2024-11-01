42b82fc1bde5ee853c7ec4beebd7a3cf59840c1a
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-01 22:19:50 +0800

    infinite lop

---
+++ Day61.java
@@ -0,0 +1,14 @@
+public class Day61 {
+    public static void main (String[]arngs){
+        for(;;){
+            System.out.println("anlimited");
+        }
+        //while(true){
+        //System.out.println("tes");
+        //}
+        
+       // do{
+        // System.out.println("tes");
+        //}while(true);
+    }
+}
