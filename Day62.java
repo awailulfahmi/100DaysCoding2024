908dc6fd8167fdab5349f75d93740e1bafc05a35
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-02 21:07:21 +0800

    break

---
+++ Day62.java
@@ -0,0 +1,12 @@
+public class Day62 {
+    public static void main(String[]arngs){
+        for (int i = 1 ; i<100 ; i++ ){
+            if(i==55){
+              System.out.println(i+"singgah koeee");
+            break;    
+            }
+          System.out.print(i+" ");
+        }
+        
+    }
+}
