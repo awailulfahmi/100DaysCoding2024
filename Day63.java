f50e74282f2a0e9e97ea5c1b43e004b5f93a3efd
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-03 23:35:14 +0800

    cotinue

---
+++ Day63.java
@@ -0,0 +1,13 @@
+
+public class Day63 {
+    public static void main(String[]arngs){
+        for (int i = 1 ; i<=100 ; i++ ){
+            if(i==55){
+              System.out.println("skip");
+            continue;    
+            }
+          System.out.print(i+" ");
+        }
+        
+    }
+}
