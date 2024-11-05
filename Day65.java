88912da2ff95cc3647c51a8af48fb544861104a5
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-05 22:04:48 +0800

    nested lop

---
+++ Day65.java
@@ -0,0 +1,16 @@
+
+package day65;
+
+public class Day65 {
+    public static void main(String[] args) {
+        int bintang = 10;
+        
+        for(int a = 1; a<=bintang; a++){
+            for(int p = 1; p<=a; p++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
