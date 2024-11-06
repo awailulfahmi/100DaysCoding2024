67a9ea3a64caf095ada28fb0b46f3ab6b8d75668
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-06 22:35:39 +0800

    Create Day66.java

---
+++ Day66.java
@@ -0,0 +1,13 @@
+
+package day64;
+
+public class Day66 {
+    public static void main(String[] args) {
+                for(int a = 1; a<=5; a++){
+            for(int p = 1; p<=a; p++){
+                System.out.print("* ");
+            }
+            System.out.println();
+    }
+}
+}
