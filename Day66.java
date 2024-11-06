415de307fae14b27655d06de2ee54704cb733d40
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-07 00:20:35 +0800

    Update Day66.java

--- Day66.java
+++ Day66.java
@@ -1,13 +1,26 @@
 
+
 package day64;
 
 public class Day66 {
     public static void main(String[] args) {
-                for(int a = 1; a<=5; a++){
-            for(int p = 1; p<=a; p++){
-                System.out.print("* ");
+        //pola persegi
+        int angka = 5;
+                for(int a = 1; a<=angka; a++){
+                for(int p = 1; p<=angka; p++){
+                System.out.print(" *");
             }
             System.out.println();
     }
+                System.out.println("--------------------");
+                int kotak = 5;
+                int kotakk = 10;
+                for(int c = 1; c<=kotak; c++){
+                for(int v = 1; v<=kotakk; v++){
+                    System.out.print(" *");        
+                    }
+                    System.out.println(); 
+                }
+                
 }
 }
