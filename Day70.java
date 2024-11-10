093429716e5ba847d80a56f4523cac1518e9ce09
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-10 23:28:23 +0800

    Create Day70.java

---
+++ Day70.java
@@ -0,0 +1,16 @@
+
+package day69;
+import java.util.Scanner;
+public class Day70 {
+public void byone (String nama){
+    System.out.println("bay one epep "+nama);
+}
+    public static void main(String[] args) {
+        Day70 a = new Day70();
+     Scanner ab = new Scanner(System.in);
+     String nama_orang = ab.nextLine();
+        a.byone(nama_orang);
+    }
+    
+ 
+}
