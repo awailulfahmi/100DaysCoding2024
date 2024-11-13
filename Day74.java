d67bd7ef34c4c2772a5389efb768a49ac935c84e
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-14 00:55:21 +0800

    Create Day74.java

---
+++ Day74.java
@@ -0,0 +1,14 @@
+
+package day69;
+import java.util.Scanner;
+public class Day74 {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner (System.in);
+        String a = ab.nextLine();
+        
+        int panjang_kalimat = a.length();
+        
+        System.out.println("panjang kalimat  "+a+" aadalah "+panjang_kalimat+" huruf");
+        
+    }
+}
