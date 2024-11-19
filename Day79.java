45850a39eb40c90e10600f501cb6e0d69c7ffeb1
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-19 21:51:08 +0800

    Create Day79.java

---
+++ Day79.java
@@ -0,0 +1,12 @@
+package day69;
+import java.util.Arrays;
+public class Day79 {
+    public static void main(String[] args) {
+    int[] a = {1,2,3,4,5};
+ 
+        System.out.println("nama teman saya"+ a.toString());
+        
+        System.out.println("nama dalam arrays"+Arrays.toString(a));
+    
+}
+}
