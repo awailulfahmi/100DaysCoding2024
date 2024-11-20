fa255ec481226df80e9bba105f69e64e2a21efcf
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-20 21:33:00 +0800

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,14 @@
+
+package day69;
+import java.util.Scanner;
+public class Day80 {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner (System.in);
+        System.out.println("masukan nama");
+        String m = ab.nextLine();
+        
+        m = m.trim();
+        
+        System.out.println("nama saya "+m);
+    }
+}
