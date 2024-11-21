69f23588944645ba750d8ff29c366837ac13f008
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-21 21:03:57 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,13 @@
+package day69;
+import java.util.Scanner;
+public class Day81 {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner (System.in);
+        System.out.println("masukan angka");
+        int a = ab.nextInt();
+        
+        String strA = String.valueOf(a);
+        
+        System.out.println("angka sebagai string"+strA);
+    }
+}
