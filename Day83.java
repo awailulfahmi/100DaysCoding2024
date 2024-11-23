7a012ec027506f53ee1a6fa2b7c8ccfd29a79fe6
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-24 00:00:51 +0800

    Create Day83.java

---
+++ Day83.java
@@ -0,0 +1,11 @@
+package day69;
+
+public class Day83 {
+
+    public static void main(String[] args) {
+        int[] tanggal = {18, 20, 3, 7};
+        for (int i = 1; i < tanggal.length; i++){
+            System.out.println("tanggal lahir "+i+" : "+tanggal[i]);
+        }
+    }
+}
