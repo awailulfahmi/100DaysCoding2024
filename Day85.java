4c6ece72305a1a9fa20f97fda7a8ad2766e788d8
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-26 00:01:25 +0800

    Create Day85.java

---
+++ Day85.java
@@ -0,0 +1,14 @@
+package day69;
+
+public class Day85 {
+
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        System.out.println("Jam masuk");
+        for (int i = 1; i < a.length; i++) {
+            System.out.println(i + ":" + a[i]);
+
+        }
+
+    }
+}
