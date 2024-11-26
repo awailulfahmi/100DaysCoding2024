0dd3e8fab4fbb85a197a449fbc6bae2f9fa9865d
awailulfahmi <awailulfahmi18012000@gmail.com>
2024-11-26 22:34:38 +0800

    Create Day86.java

---
+++ Day86.java
@@ -0,0 +1,13 @@
+package day69;
+
+public class Day86 {
+
+    public static void main(String[] args) {
+        String[] nama = {"awal", "niel", "kriel", "ari"};
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println("nama" + i + " : " + nama[i]);
+        }
+
+    }
+
+}
